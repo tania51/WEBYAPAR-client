3d821d5895458eb91b0996f1716c74880b3d8942
Tania Akter Bristy <taniaakterbristybd@gmail.com>
2024-01-17 07:03:05 +0600

    admin form data post to the MongoDB database successfully

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,31 @@ const adminUserIdElement = document.getElementById('user-id');
 const adminUserPassElement = document.getElementById('user-pass');
 
 
-document.getElementById('admin-login-btn').addEventListener('click', function(event) {
+document.getElementById('admin-login-btn').addEventListener('click', function (event) {
     event.preventDefault();
     if (adminUserIdElement && adminUserPassElement) {
         const adminUserId = adminUserIdElement.value;
         const adminUserPassValue = adminUserPassElement.value;
-        console.log(adminUserId, adminUserPassValue);
-      } else {
+        const role = 'admin';
+
+        const adminLoginData = {
+            adminId: adminUserId,
+            adminPassword: adminUserPassValue,
+            role: role
+        };
+
+        fetch('http://localhost:3000/admin', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(adminLoginData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    } else {
         console.error('Element with ID "user-id" not found');
-      }
+    }
 })
